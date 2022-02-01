642426b17626ec574764ddc4e23cb7d6a93c9791
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-02-01 15:56:34 +0330

    Update script.js
    pop up functions are not complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const mobileMenu = document.querySelector('.mobile-menu');
+// hamburger menu
 const humberger = document.querySelector('.humberger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cancel = document.querySelector('.cancel');
@@ -17,4 +17,40 @@ cancel.addEventListener('click', close);
 
 for (let i = 0; i < lists.length; i += 1) {
   lists[i].addEventListener('click', close);
+}
+
+// pop-up 
+
+const popups = [{
+  id: 'con-1',
+  name: 'Tonic',
+  desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and .",
+  mobileImage: '',
+  desktopImage: '',
+  skillSet: updateSkills('html', 'css', 'javaScript'),
+  jobProfile: createProfile('CANOPY', 'Back End Dev', 2018),
+},
+{
+  id: 'con-2',
+  name: 'Tonic',
+  desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and .",
+  mobileImage: '',
+  desktopImage: '',
+  skillSet: updateSkills('html', 'css', 'javaScript'),
+  jobProfile: createProfile('CANOPY', 'Back End Dev', 2018),
+},
+{
+  id: 'con-1',
+  name: 'Tonic',
+  desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and .",
+  mobileImage: '',
+  desktopImage: '',
+  skillSet: updateSkills('html', 'css', 'javaScript'),
+  jobProfile: createProfile('CANOPY', 'Back End Dev', 2018),
+},
+
+}];
+
+function updateSkills(skill1 , skill2, skill){
+
 }
