9a606bcd2d38057d9e51ffd17efe68c180c36652
moise mulungu <90564431+moise-mulungu@users.noreply.github.com>
2022-02-02 15:51:47 +0200

    Fixing the trailing comma

--- popup-script.js
+++ popup-script.js
@@ -1,44 +1,44 @@
-
 const detail = [{
   title: 'Multi-Post Stories',
   image: ['popup-mobile.svg', 'popup-desktop.svg'],
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",technologies: ['HTML' , 'CSS', 'Javascript']
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown", 
+  technologies: ['HTML', 'CSS', 'Javascript'],
 },
 {
   title: 'Profesional Art Printing Data',
   image: ['popup-mobile.svg', 'popup-desktop.svg'],
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
-  technologies: ['HTML', 'CSS', 'Javascript']
+  technologies: ['HTML', 'CSS', 'Javascript'],
 },
 {
   title: 'Profesional Art Printing Data',
   image: ['popup-mobile.svg', 'popup-desktop.svg'],
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
-  technologies: ['HTML', 'CSS', 'Javascript']
+  technologies: ['HTML', 'CSS', 'Javascript'],
 },
 {
   title: 'Profesional Art Printing Data',
   image: ['popup-mobile.svg', 'popup-desktop.svg'],
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
-  technologies: ['HTML', 'CSS', 'Javascript']
+  technologies: ['HTML', 'CSS', 'Javascript'],
 },
 {
   title: 'Profesional Art Printing Data',
   image: ['popup-mobile.svg', 'popup-desktop.svg'],
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
-  technologies: ['HTML', 'CSS', 'Javascript']
+  technologies: ['HTML', 'CSS', 'Javascript'],
 },
 {
   title: 'Profesional Art Printing Data',
   image: ['popup-mobile.svg', 'popup-desktop.svg'],
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
-  technologies: ['HTML', 'CSS', 'Javascript']
+  technologies: ['HTML', 'CSS', 'Javascript'],
 },
 {
   title: 'Profesional Art Printing Data',
   image: ['popup-mobile.svg', 'popup-desktop.svg'],
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
-  technologies: ['HTML', 'CSS', 'Javascript']
+  technologies: ['HTML', 'CSS', 'Javascript'],
 }];
 
 const overlay = document.getElementById('overlay');
@@ -48,7 +48,7 @@ popUp.classList.add('pop-up');
 
 function popupWin(order) {
   popUp.style.display = 'block';
-  overlay.style.opacity = 1 ;
+  overlay.style.opacity = 1;
   document.querySelector('.pop-up-title').innerHTML = `<h3 class="post">${detail[order].title}</h3>`;
   document.querySelector('.pop-up-tech').innerHTML = `<ul class= "tech"> 
   <li>${detail[order].technologies[0]}</li>
