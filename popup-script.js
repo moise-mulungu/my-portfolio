5aebce4ef6779ec3accce8de3df4a0a3e2ab6e75
moise mulungu <90564431+moise-mulungu@users.noreply.github.com>
2022-02-02 15:29:25 +0200

    Update popup-script.js

--- popup-script.js
+++ popup-script.js
@@ -1,9 +1,8 @@
-// pop-up 
+
 const detail = [{
   title: 'Multi-Post Stories',
   image: ['popup-mobile.svg', 'popup-desktop.svg'],
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
-  technologies: ['HTML' , 'CSS', 'Javascript']
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",technologies: ['HTML' , 'CSS', 'Javascript']
 },
 {
   title: 'Profesional Art Printing Data',
@@ -40,26 +39,14 @@ const detail = [{
   image: ['popup-mobile.svg', 'popup-desktop.svg'],
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
   technologies: ['HTML' , 'CSS', 'Javascript']
-}]
+}];
 
-let overlay = document.getElementById('overlay');
-let btn = document.querySelectorAll('.click , .click-two');
-let popUp = document.getElementById('pop-up');
+const overlay = document.getElementById('overlay');
+const btn = document.querySelectorAll('.click , .click-two');
+const popUp = document.getElementById('pop-up');
 popUp.classList.add('pop-up');
 
-// btn.forEach (button => {
-//   button.addEventListener('click',() => {
-//     popupWin(parseInt(btn[button].id));
-//   })
-// })
-
-for (let i = 0; i < btn.length ; i++) {
-  btn[i].addEventListener('click', () => {
-    popupWin(i);
-  });
-}
-
-function popupWin (order) {
+function popupWin(order) {
   popUp.style.display = 'block';
   overlay.style.opacity = 1 ;
   document.querySelector('.pop-up-title').innerHTML = `<h3 class="post">${detail[order].title}</h3>`;
@@ -71,9 +58,15 @@ function popupWin (order) {
   document.querySelector('.pop-up-text').innerHTML = `<p>${detail[order].description}</p>`
 }
 
-const close = document.getElementById('close-button')
+for (let i = 0; i < btn.length; i=i+1) {
+  btn[i].addEventListener('click', () => {
+    popupWin(i);
+  });
+}
+
+const close = document.getElementById('close-button');
 close.addEventListener('click', () => {
-    popUp.style.display = 'none';
-    overlay.style.opacity = 0 ;
-  })
+  popUp.style.display = 'none;
+  overlay.style.opacity = 0;
+  });
 
