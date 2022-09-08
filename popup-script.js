03d7f3c63bde11ad698064240a4820eaf9462031
moise <moisemlg90@gmail.com>
2022-09-08 05:02:55 +0200

    Fix linters

--- popup-script.js
+++ popup-script.js
@@ -1,7 +1,7 @@
 const detail = [{
   title: 'E-Pay',
   image: ['popup-mobile.svg', 'popup-desktop.svg'],
-  description: "This is a web application that helps you to manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.",
+  description: 'This is a web application that helps you to manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
   technologies: ['HTML', 'CSS', 'Ruby on Rails'],
 },
 {
